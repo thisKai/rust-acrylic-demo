9cb23c5351cc0f97bcd676090b669638a1f56c95
kai <thiskaiguy@gmail.com>
2020-12-30 05:22:47 +0000

    Install Win2D

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,5 +1,8 @@
 fn main() {
     winrt::build!(
+        microsoft::graphics::canvas::*
+        microsoft::graphics::canvas::effects::*
+        microsoft::graphics::canvas::ui::composition::*
         windows::foundation::numerics::{Vector2, Vector3}
         windows::foundation::TimeSpan
         windows::graphics::SizeInt32
@@ -15,8 +18,10 @@ fn main() {
             Compositor,
             ContainerVisual,
             SpriteVisual,
+            CompositionEffectSourceParameter,
         }
         windows::ui::composition::desktop::DesktopWindowTarget
         windows::ui::Colors
+        windows::storage::streams::{InMemoryRandomAccessStream, IBuffer, DataWriter}
     );
 }
